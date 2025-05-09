da843186b3d1d8df4cb8ad6c2294a5b8b11312d3
judgeOstrich <jeraldnyaga15@gmail.com>
2025-05-10 01:49:28 +0300

    Complete fibonnacii

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    first = 0
+    second = 1
+    fibo = []
+
+    if length == 1:
+        fibo.append(first)
+    elif length == 2:
+        fibo.append(first)
+        fibo.append(second)
+    else:
+        while len(fibo) < length:
+            fibo.append(first)
+            first = second
+            second = second + fibo[-1]
+    print(fibo)
+
